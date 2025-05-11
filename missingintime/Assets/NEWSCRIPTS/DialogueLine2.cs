6bc3af72457b9b5eda87309550131d1973542747
jesspancakes <c24453362@mytudublin.ie>
2025-05-11 13:37:31 +0100

    opening scene dialogue DONE!

---
+++ missingintime/Assets/NEWSCRIPTS/DialogueLine2.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private Text textHolder;
+
+        [Header("Text Options")]
+        [SerializeField] private string input;
+        [SerializeField] private Color textColor;
+        [SerializeField] private Font textFont;
+
+        [Header("Time parameters")]
+        [SerializeField] private float delay;
+        [SerializeField] private float delayBetweenLines;
+
+        [Header("Sound")]
+        [SerializeField] private AudioClip sound;
+
+        [Header("Character Image")]
+        [SerializeField] private Sprite characterSprite;
+        [SerializeField] private Image imageHolder;
+
+        private void Awake()
+        {
+            textHolder = GetComponent<Text>();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(WriteText(input, textHolder, textColor, textFont, delay, sound, delayBetweenLines));
+            imageHolder.sprite = characterSprite;
+            imageHolder.preserveAspect = true;
+        }
+    }
+}
+
